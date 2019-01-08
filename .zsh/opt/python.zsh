946359615a0ca57ba6e2970950142539b590007c
OpenJNY <openjny@gmail.com>
2019-01-09 03:34:21 +0900

    Fix zsh alias for python

--- .zsh/opt/python.zsh
+++ .zsh/opt/python.zsh
@@ -1,3 +1,9 @@
+# Update pip package
+function pip-update-all() {
+    echo "which pip: $(which pip)"
+    echo "pip list --outdated --format=columns | awk '{if (NR>2) print $1}' | xargs pip install -U"
+    pip list --outdated --format=columns | awk '{if (NR>2) print $1}' | xargs pip install -U
+}
 
 # zsh のディレクトリの変更時に chpwd 関数が呼ばれる仕様を利用して
 # 自動で vertualenv をアクティベートする
@@ -13,3 +19,6 @@ chpwd_pyvenv # for initialization
 add-zsh-hook chpwd chpwd_pyvenv
 
 # alias venv='source venv/bin/activate'
+
+# Jupyter Notebook
+alias jupyter-notebook-silent='jupyter notebook --no-browser'
